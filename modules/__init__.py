af0c15229d82ce4aab933b4f1c6b5e6141f2f8c6
raroiipgs58985-bit <raroiipgs58985@gmail.com>
2026-07-24 04:58:55 +0500

    feat: enable persistent slot statistics

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,4 +1,4 @@
-from .economy import EconomyManager
+from .economy_with_slot_stats import EconomyManager
 from .site_agent_client import SiteAgentClient
 
 __all__ = ["EconomyManager", "SiteAgentClient"]
